263778a62ddf5dc9b64ba61a6316bb3695ab618c
francisco ameri <franameri2016@gmail.com>
2023-11-15 18:42:09 -0300

    cambio endpoint POST Temas, para que se le pase valor string

--- parcial3/src/main/java/ar/edu/ubp/das/rest/controllers/ParcialController.java
+++ parcial3/src/main/java/ar/edu/ubp/das/rest/controllers/ParcialController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.edu.ubp.das.rest.beans.SugerenciaBean;
@@ -33,20 +34,25 @@ public class ParcialController {
 	@Autowired
     SoapServiceClient clienteSOAP;
 
+	@GetMapping(path="/tipo")
+    public ResponseEntity<List<TipoServiciosBean>> getTipoServicio() {
+      return new ResponseEntity<>(repository.getTipoServicio(), HttpStatus.OK);
+    }
+	/*
 	@GetMapping("/tipos")
     public List<TipoServiciosBean> getTipos() {
     	List<TipoServiciosBean> temp = clienteSOAP.getTipoServicios();
     	return temp;
     }
 	 
-    @PostMapping(
-    	      path="/temas_servicios",
-    	      consumes={MediaType.MULTIPART_FORM_DATA_VALUE}
-    	    )
-    	    public ResponseEntity<List<TemaServicioBean>> getTemasServicios(TemaServicioCriteriaBean criteria) {
-    	        return new ResponseEntity<>(repository.getTemasServicios(criteria), HttpStatus.OK);
-    	    }
+    
+*/
 
+@PostMapping(path = "/temas", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+public ResponseEntity<List<TemaServicioBean>> getTemas(@RequestParam("codTipoServicio") String codTipoServicio) {
+    return new ResponseEntity<>(repository.getTemasServicios(codTipoServicio), HttpStatus.OK);
+}
+	
 
     
 
--- parcial3/src/main/java/ar/edu/ubp/das/rest/repositories/ParcialRepository.java
+++ parcial3/src/main/java/ar/edu/ubp/das/rest/repositories/ParcialRepository.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
+import ar.edu.ubp.das.rest.beans.TipoServiciosBean;
 import ar.edu.ubp.das.rest.beans.SugerenciaBean;
 import ar.edu.ubp.das.rest.beans.TemaServicioBean;
 import ar.edu.ubp.das.rest.beans.TemaServicioCriteriaBean;
@@ -22,12 +23,33 @@ import ar.edu.ubp.das.rest.beans.TemaServicioCriteriaBean;
 public class ParcialRepository {
 	 @Autowired
 	    private JdbcTemplate jdbcTpl;
-	    
-	  
+	 
+		@SuppressWarnings("unchecked")
+	@Transactional
+	public List<TemaServicioBean> getTemasServicios(String codigo) {
+       	System.out.println(codigo);
+
+        SqlParameterSource in = new MapSqlParameterSource()
+                .addValue("cod_tipo_servicio", codigo);
+        
+    	SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTpl)
+    	   .withProcedureName("get_temas_tipo_servicio")
+           .withSchemaName("dbo")
+       	   .returningResultSet("tipos_servicios", BeanPropertyRowMapper.newInstance(TemaServicioBean.class));
+       	
+       	Map<String, Object> out = jdbcCall.execute(in);
+       	System.out.println(out);
+       	return (List<TemaServicioBean>)out.get("tipos_servicios");
+	}
+	/* 
 	 @SuppressWarnings("unchecked")
-	    public List<TemaServicioBean> getTemasServicios(TemaServicioCriteriaBean criteria) {
+	    public List<TemaServicioBean> getTemasServicios(String codigo) {
+			System.out.println("---------------REPO------------------------");
+				System.out.println(codigo);
+			System.out.println("---------------------------------------");
+				
 	        SqlParameterSource in = new MapSqlParameterSource()
-	           .addValue("cod_tipo_servicio", criteria.getCod_tipo_servicio());
+	           .addValue("cod_tipo_servicio", codigo);
 
 	        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTpl)
 	           .withProcedureName("get_temas_tipo_servicio")
@@ -37,10 +59,7 @@ public class ParcialRepository {
 	        Map<String, Object> out = jdbcCall.execute(in);
 	        return (List<TemaServicioBean>)out.get("temas_tipo_servicio");
 	    }
-	 
-	 
-	 
-	 
+	 */
 	 
 	 @Transactional
 	    public SugerenciaBean insSugerencia(SugerenciaBean data) {
@@ -53,9 +72,17 @@ public class ParcialRepository {
 	        SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTpl)
 	           .withProcedureName("ins_sugerencia")
 	           .withSchemaName("dbo");
-	        jdbcCall.execute(in);
-
-	        
+	        jdbcCall.execute(in);	        
 	        return data;
 	    }
+
+		@SuppressWarnings("unchecked")public List<TipoServiciosBean> getTipoServicio() {
+			SimpleJdbcCall jdbcCall = new SimpleJdbcCall(jdbcTpl)
+			   .withProcedureName("get_tipos_servicios")
+			   .withSchemaName("dbo") 
+				  .returningResultSet("tipo_servicios", BeanPropertyRowMapper.newInstance(TipoServiciosBean.class));
+			   
+			   Map<String, Object> out = jdbcCall.execute();
+			   return (List<TipoServiciosBean>)out.get("tipo_servicios");
+		}
 }
